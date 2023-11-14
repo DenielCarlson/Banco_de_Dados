be6a797e7497be210244ae44d7e8e3524158bb0d
Deniel Carlson <denielcarlson_18@outlook.com>
2023-11-14 16:06:28 -0300

    Criando as tabelas cliente, vendedor e produto

--- Fisico/Script_Ze_Delivery.sql
+++ Fisico/Script_Ze_Delivery.sql
@@ -1,3 +1,6 @@
+-- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
 -- TABELAS 
 -- ------------------------------------------------------------------
 -- TABELA DAS LOJAS (FILIAIS)
@@ -12,6 +15,7 @@ CONSTRAINT [PK_LOJA] PRIMARY KEY([COD_LOJA] AUTOINCREMENT)
 
 CREATE UNIQUE INDEX IF NOT EXISTS [IPK_LOJA] ON LOJA([COD_LOJA]);
 
+-- ------------------------------------------------------------------
 -- ------------------------------------------------------------------
 -- TABELA DOS FORNECEDORES
 CREATE TABLE IF NOT EXISTS [FORNECEDOR]
@@ -28,6 +32,7 @@ CONSTRAINT [PK_FORNECEDOR] PRIMARY KEY([COD_FORNECEDOR] AUTOINCREMENT)
 CREATE UNIQUE INDEX IF NOT EXISTS [IPK_FORNECEDOR] ON FORNECEDOR([COD_FORNECEDOR]);
 
 -- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
 -- TABELA DE USUÁRIOS 
 CREATE TABLE IF NOT EXISTS [USUARIO]
 (
@@ -42,6 +47,60 @@ CREATE UNIQUE INDEX IF NOT EXISTS [IPK_USUARIO] ON USUARIO([COD_USUARIO]);
 
 -- ------------------------------------------------------------------
 -- ------------------------------------------------------------------
+-- TABELA DO CLIENTE
+CREATE TABLE IF NOT EXISTS [CLIENTE]
+(
+[COD_CLIENTE] INTEGER NOT NULL,
+[COD_USUARIO] INTEGER NOT NULL,
+[NOME] VARCHAR(100) NOT NULL,
+[CPF] VARCHAR(20),
+[TELEFONE] VARCHAR(20) NOT NULL,
+[ENDERECO] VARCHAR(50) NOT NULL,
+CONSTRAINT [PK_CLIENTE] PRIMARY KEY([COD_CLIENTE] AUTOINCREMENT),
+CONSTRAINT [FK_USUARIO] FOREIGN KEY([COD_USUARIO]) REFERENCES USUARIO([COD_USUARIO])
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS [IPK_CLIENTE] ON CLIENTE([COD_CLIENTE]);
+
+-- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
+-- TABELA DO VENDEDOR
+CREATE TABLE IF NOT EXISTS [VENDEDOR]
+(
+[COD_VENDEDOR] INTEGER NOT NULL,
+[COD_USUARIO] INTEGER NOT NULL,
+[COD_LOJA] INTEGER NOT NULL,
+[NOME] VARCHAR(100) NOT NULL,
+[CPF] VARCHAR(20) NOT NULL,
+[TELEFONE] VARCHAR(20) NOT NULL,
+[ENDERECO] VARCHAR(50) NOT NULL,
+[ISADMIN] BOOLEAN NOT NULL,
+CONSTRAINT [PK_VENDEDOR] PRIMARY KEY([COD_VENDEDOR] AUTOINCREMENT),
+CONSTRAINT [FK_USUARIO] FOREIGN KEY([COD_USUARIO]) REFERENCES USUARIO([COD_USUARIO]),
+CONSTRAINT [FK_LOJA] FOREIGN KEY([COD_LOJA]) REFERENCES LOJA([COD_LOJA])
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS [IPK_VENDEDOR] ON VENDEDOR([COD_VENDEDOR]);
+
+-- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
+-- TABELA DO PRODUTO
+CREATE TABLE IF NOT EXISTS [PRODUTO]
+(
+[COD_PRODUTO] INTEGER NOT NULL,
+[COD_FORNECEDOR] INTEGER NOT NULL,
+[NOME] VARCHAR(100) NOT NULL,
+[VLR_PRODUTO] DOUBLE NOT NULL,
+CONSTRAINT [PK_PRODUTO] PRIMARY KEY([COD_PRODUTO] AUTOINCREMENT),
+CONSTRAINT [FK_FORNECEDOR] FOREIGN KEY([COD_FORNECEDOR]) REFERENCES FORNECEDOR([COD_FORNECEDOR])
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS[IPK_PRODUTO] ON PRODUTO([COD_PRODUTO]);
+
+
+
+-- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
 -- ------------------------------------------------------------------
 -- SELECTS 
 -- ------------------------------------------------------------------
@@ -49,12 +108,29 @@ CREATE UNIQUE INDEX IF NOT EXISTS [IPK_USUARIO] ON USUARIO([COD_USUARIO]);
 SELECT * FROM LOJA;
 
 -- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
 --SELECT DOS REGISTROS DA TABELA FORNECEDOR
 SELECT * FROM FORNECEDOR;
 
 -- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
 -- SELECT DOS REGISTROS DA TABELA USUARIO
 SELECT * FROM USUARIO;
+
+-- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
+-- SELECT DOS REGISTROS DA TABELA CLIENTE
+SELECT * FROM CLIENTE;
+
+-- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
+-- SELECT DOS REGISTROS DA TABELA VENDEDOR
+SELECT * FROM VENDEDOR;
+
+-- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
+-- SELECT DOS REGISTROS DA TABELA PRODUTO
+SELECT * FROM PRODUTO;
 
 -- ------------------------------------------------------------------
 -- ------------------------------------------------------------------
@@ -65,9 +141,26 @@ SELECT * FROM USUARIO;
 DROP TABLE IF EXISTS [LOJA];
 
 -- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
 -- DROP DA TABELA FORNECEDOR
 DROP TABLE IF EXISTS [FORNECEDOR];
 
 -- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
 -- DROP DA TABELA USUARIO
 DROP TABLE IF EXISTS [USUARIO];
+
+-- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
+-- DROP NA TABELA CLIENTE
+DROP TABLE IF EXISTS [CLIENTE]; 
+
+-- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
+--DROP NA TABELA VENDEDOR
+DROP TABLE IF EXISTS [VENDEDOR];
+
+-- ------------------------------------------------------------------
+-- ------------------------------------------------------------------
+-- DROP NA TABELA PRODUTO
+DROP TABLE IF EXISTS [PRODUTO];
